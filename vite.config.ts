292e16d9b6b8b65e0717349a9e6604595d422afc
Himanshu Gupta <shg9648@hotmail.com>
2025-05-12 14:24:32 +0530

    new commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/latest_portfolio/', // ✅ This is necessary for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
